6db50c2b2dd76d69cb930f12783c565595b5c55e
makewheels <makewheels>
2021-04-04 22:48:50 +0800

    拦截器

---
+++ src/main/java/com/example/testspringbootinterceptor/LoginInterceptor.java
@@ -0,0 +1,18 @@
+package com.example.testspringbootinterceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/example/testspringbootinterceptor/TestSpringbootInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspringbootinterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringbootInterceptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringbootInterceptorApplication.class, args);
+    }
+
+}
